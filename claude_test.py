79dd9de3989f1f9e5dff50ac32eaf759cb215f17
Swayam Chidrawar <schidraw@calpoly.edu>
2023-12-28 21:09:55 -0800

    got rid of secrets

--- claude_test.py
+++ claude_test.py
@@ -32,15 +32,3 @@ def main():
     print('\n\n')
 if __name__ == '__main__':
     main()
-# import boto3
-# polly_client = boto3.Session(
-#                 aws_access_key_id='AKIAWTQ5AADJWKD5FC43',
-#     aws_secret_access_key='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',
-#     region_name='us-west-2').client('polly')
-# response = polly_client.synthesize_speech(VoiceId='Joanna',
-#                 OutputFormat='mp3',
-#                 Text = 'This is a sample text to be synthesized.',
-#                 Engine = 'neural')
-# file = open('speech.mp3', 'wb')
-# file.write(response['AudioStream'].read())
-# file.close()
